4edc777ac137971703ea0409fa873aa3ed7a418e
Evgeniy Klimovich <kliasanik@gmail.com>
2026-02-26 21:02:45 +0300

    create getStatus

--- state/data.js
+++ state/data.js
@@ -1,7 +1,10 @@
 import {GAME_STATUSES} from "./GAME_STATUSES";
 
-const _state ={
-  status : GAME_STATUSES.SETTINGS,
+const _state = {
+  status: GAME_STATUSES.SETTINGS,
 
 }
 
+export function getStatus() {
+  return _state.status
+}
