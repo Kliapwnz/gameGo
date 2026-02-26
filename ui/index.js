07ba1bf12123793c57f1ec3061547e21b1b713ac
Evgeniy Klimovich <kliasanik@gmail.com>
2026-02-26 21:27:23 +0300

    game-mode.component.js and lose-mode.component.js visible

--- ui/index.js
+++ ui/index.js
@@ -1,6 +1,8 @@
 import {getStatus} from "../state/data.js";
 import {GAME_STATUSES} from "../state/GAME_STATUSES.js";
 import {SettingsMode} from "./settings-mode.component.js";
+import {GamesMode} from "./game-mode.component.js";
+import {LoseMode} from "./lose-mode.component.js";
 
 const status = getStatus()
 
@@ -10,6 +12,14 @@ switch (status) {
   case GAME_STATUSES.SETTINGS :
     const settingsModeElement = SettingsMode()
     rootElement.append(settingsModeElement)
+    break;
+  case GAME_STATUSES.IN_PROGRESS :
+    const inProgressModeElement = GamesMode()
+    rootElement.append(inProgressModeElement)
+    break;
+  case GAME_STATUSES.LOSE :
+    const LoseModeElement = LoseMode()
+    rootElement.append(LoseModeElement)
     break;
   default:
     rootElement.append(status)
