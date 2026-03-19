5fe40cf979fc69b5b17fb05f5ca2075e98c6f65b
Evgeniy Klimovich <kliasanik@gmail.com>
2026-03-19 22:25:22 +0300

    use cell component

--- ui/game-mode/grid.component.js
+++ ui/game-mode/grid.component.js
@@ -1,4 +1,5 @@
-import {getGooglePosition, getGridSize, getPlayer1Position} from "../../state/data.js";
+import {getGridSize} from "../../state/data.js";
+import {Cell} from "./cell.component";
 
 export const Grid = () => {
   const element = document.createElement('table')
@@ -15,21 +16,13 @@ export const Grid = () => {
 Grid.render = (element) => {
   element.innerHTML = ""
   const gridSize = getGridSize()
-  const googlePosition = getGooglePosition()
-  const player1Position = getPlayer1Position()
+
   for (let y = 0; y < gridSize.rowsCount; y++) {
     const row = document.createElement('tr')
 
     for (let x = 0; x < gridSize.columnsCount; x++) {
-      const cell = document.createElement('td')
-      if (x === googlePosition.x && y === googlePosition.y) {
-        cell.append('G')
-        row.append(cell)
-      }
-      if (x === player1Position.x && y === player1Position.y) {
-        cell.append('🚶‍')
-      }
-      row.append(cell)
+      const cellInstance = Cell(x, y)
+      row.append(cellInstance.element)
     }
     element.append(row)
   }
